1e56339b811c6263a632e9045b8650c7e4cf44a3
divyavis <61997492+divyavis@users.noreply.github.com>
2020-04-20 20:56:32 -0500

    Add files via upload

---
+++ Term Project/journalReading.py
@@ -0,0 +1,95 @@
+class JournalSetup(object):
+    def __init__(self):
+        self.journal = ""
+        #list from https://www.english-grammar-revolution.com/list-of-conjunctions.html
+        self.coordinatingConjunctions = ['for', 'and','nor', 'but', 'or', 'yet', 'so']
+        self.subordinatingConjunctions = ['after', 'although', 'because', 'before', 'lest', 'once', 'only', 'since', 'than', 'that', 'though', 'till', 'unless', 'until', 'when', 'whenever', 'where', 'wherever', 'while']
+        self.correlativeConjunctions = ['both', 'either', 'neither', 'nor', 'only', 'also', 'whether']
+        #list from https://www.talkenglish.com/vocabulary/top-50-prepositions.aspx
+        self.popularPrepositions = ['with', 'from', 'into', 'during', 'including', 'against', 'among', 'throughout', 'despite', 'towards', 'toward', 'upon', 'about', 'through', 'between', 'without', 'along', 'except', 'out']
+        self.nonwords = self.coordinatingConjunctions + self.subordinatingConjunctions + self.correlativeConjunctions + self.popularPrepositions
+        self.relevantWords = []
+        self.songScores = []
+    
+    def readSingleEntry(self, path):
+        try:
+            with open(path, "rt") as f:
+                self.journal = f.read()
+                if len(self.journal) < 10:
+                    return None
+        except:
+            return None
+
+    def getRelevantWords(self):
+        self.journal = self.journal.lower()
+        for word in self.journal.split():
+            cleanWord = ""
+            for letter in word:
+                if letter.isalnum():
+                    cleanWord += letter
+            if cleanWord not in self.nonwords and len(cleanWord) >= 3:
+                self.relevantWords.append(cleanWord)
+
+    def countWordOccurrencesInSong(self, lyricsDict):
+        self.wordCountsDict = dict()
+        for song in lyricsDict:
+            self.wordCountsDict[song] = dict()
+            wordCounts = self.wordCountsDict[song]
+            for word in self.relevantWords:
+                count = 0
+                lyrics = lyricsDict[song]
+                for lyric in lyrics.split():
+                    if lyric == word:
+                        count += 1
+                wordCounts[word] = count
+            self.wordCountsDict[song] = wordCounts
+        return self.wordCountsDict
+
+    def getKeywordsUsed(self):
+        keywords = []
+        for song in self.wordCountsDict:
+            wordCounts = self.wordCountsDict[song]
+            for word in wordCounts:
+                if word not in keywords:
+                    keywords.append(word)
+        return keywords
+
+    def scoreSongs(self):
+        for song in self.wordCountsDict:
+            totalScore = 0
+            title = song[0]
+            url = song[1]
+            wordCounts = self.wordCountsDict[song]
+            for word in wordCounts:
+                totalScore += wordCounts[word]
+            self.songScores.append((title, url, totalScore))
+
+    #modified from https://www.cs.cmu.edu/~112/notes/notes-recursion-part1.html#mergesort
+    def merge(self, A, B):
+        C = [ ]
+        i = j = 0
+        while ((i < len(A)) or (j < len(B))):
+            if ((j == len(B)) or ((i < len(A)) and (A[i][2] >= B[j][2]))):
+                C.append(A[i])
+                i += 1
+            else:
+                C.append(B[j])
+                j += 1
+        return C
+
+    #uses common mergeSort function
+    def rankSongs(self):
+        if len(self.songScores) < 2:
+            return self.songScores
+        else:
+            mid = len(self.songScores)//2
+            left = rankSongs(self.songScores[:mid])
+            right = rankSongs(self.songScores[mid:])
+            return self.merge(left, right)
+
+    def eliminateNonMatches(self, rankedSongs):
+        updatedSongs = []
+        for song in rankedSongs:
+            if song[2] > 0:
+                updatedSongs.append(song)
+        return updatedSongs
